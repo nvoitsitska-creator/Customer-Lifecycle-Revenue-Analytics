5fefd1861bc659343fe250b23430b00e01d125f3
Anastasiia Voitsitska <nvoitsitska@gmail.com>
2026-03-10 20:27:30 +0200

    Delete Script.sql

--- Script.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE employee (
-employee_id INT PRIMARY KEY,
-first_name VARCHAR(50) NOT NULL,
-last_name VARCHAR(50) NOT NULL,
-email VARCHAR(100) UNIQUE NOT NULL,
-phone_number VARCHAR(20),
-hire_date DATE,
-salary INT
-);
